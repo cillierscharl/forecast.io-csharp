8a8e9a02a305db2ddb1760af620625e6fb9b9916
Charl Cilliers <cilliers.charl@gmail.com>
2013-04-20 16:35:50 +0200

    Initial Commit

---
+++ forecast.io-csharp/Entities/ForecastIOResponse.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Forecastio
+{
+    [DataContract]
+    public class ForecastioReponse
+    {
+        [DataMember]
+        public float latitude { get; set; }
+        [DataMember]
+        public float longitude { get; set; }
+        [DataMember]
+        public string timezone { get; set; }
+        [DataMember]
+        public int offset { get; set; }
+        [DataMember]
+        public Currently currently { get; set; }
+        [DataMember]
+        public Minutely minutely { get; set; }
+        [DataMember]
+        public Hourly hourly { get; set; }
+        [DataMember]
+        public Daily daily { get; set; }
+        [DataMember]
+        public Flags flags { get; set; }
+    }
+    [DataContract]
+    public class Currently
+    {
+        [DataMember]
+        public int time { get; set; }
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public float precipIntensity { get; set; }
+        [DataMember]
+        public float temperature { get; set; }
+        [DataMember]
+        public float dewPoint { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float cloudCover { get; set; }
+        [DataMember]
+        public float humidity { get; set; }
+        [DataMember]
+        public float pressure { get; set; }
+        [DataMember]
+        public float visibility { get; set; }
+        [DataMember]
+        public float ozone { get; set; }
+    }
+    [DataContract]
+    public class MinuteForecast
+    {
+        [DataMember]
+        public int time { get; set; }
+        [DataMember]
+        public float precipIntensity { get; set; }
+    }
+    [DataContract]
+    public class Minutely
+    {
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public List<MinuteForecast> data { get; set; }
+    }
+    [DataContract]
+    public class HourForecast
+    {
+        [DataMember]
+        public int time { get; set; }
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public float precipIntensity { get; set; }
+        [DataMember]
+        public float temperature { get; set; }
+        [DataMember]
+        public float dewPoint { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float cloudCover { get; set; }
+        [DataMember]
+        public float humidity { get; set; }
+        [DataMember]
+        public float pressure { get; set; }
+        [DataMember]
+        public float visibility { get; set; }
+        [DataMember]
+        public float ozone { get; set; }
+    }
+    [DataContract]
+    public class Hourly
+    {
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public List<HourForecast> data { get; set; }
+    }
+    [DataContract]
+    public class DailyForecast
+    {
+        [DataMember]
+        public int time { get; set; }
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public int sunriseTime { get; set; }
+        [DataMember]
+        public int sunsetTime { get; set; }
+        [DataMember]
+        public float precipIntensity { get; set; }
+        [DataMember]
+        public float precipIntensityMax { get; set; }
+        [DataMember]
+        public float temperatureMin { get; set; }
+        [DataMember]
+        public int temperatureMinTime { get; set; }
+        [DataMember]
+        public float temperatureMax { get; set; }
+        [DataMember]
+        public int temperatureMaxTime { get; set; }
+        [DataMember]
+        public float dewPoint { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float cloudCover { get; set; }
+        [DataMember]
+        public float humidity { get; set; }
+        [DataMember]
+        public float pressure { get; set; }
+        [DataMember]
+        public float visibility { get; set; }
+        [DataMember]
+        public float ozone { get; set; }
+    }
+    [DataContract]
+    public class Daily
+    {
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+        [DataMember]
+        public List<DailyForecast> data { get; set; }
+    }
+    [DataContract]
+    public class Flags
+    {
+        [DataMember]
+        public List<string> sources { get; set; }
+        [DataMember]
+        public List<string> isd_stations { get; set; }
+        [DataMember]
+        public List<string> lamp_stations { get; set; }
+        [DataMember]
+        public List<string> metar_stations { get; set; }
+        [DataMember]
+        public List<string> darksky_stations { get; set; }
+        [DataMember]
+        public string units { get; set; }
+    }
+}
---
+++ forecast.io-csharp/Forecastio.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace Forecastio
+{
+    public class Forecastio
+    {
+        private string apiKey;
+        private string latitude;
+        private string longitude;
+        private DateTime time;
+
+        private string currentForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2}";
+        private string periodForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2},{3}";
+
+        public Forecastio(string _apiKey, float _lat, float _long)
+        {
+            apiKey = _apiKey;
+            latitude = _lat.ToString(CultureInfo.InvariantCulture);
+            longitude = _long.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public Forecastio(string _apiKey, float _lat, float _long, DateTime _time)
+        {
+            apiKey = _apiKey;
+            latitude = _lat.ToString(CultureInfo.InvariantCulture);
+            longitude = _long.ToString(CultureInfo.InvariantCulture);
+            time = _time;
+        }
+
+        public ForecastioReponse Get()
+        {
+            var client = new WebClient();
+            var url = "";
+            if (time == DateTime.MinValue)
+            {
+                url = String.Format(currentForecastURL, apiKey, latitude, longitude);
+            }
+            else
+            {
+                url = String.Format(periodForecastURL, apiKey, latitude, longitude, time);
+            }
+
+            var stringResult = FormatResponse(client.DownloadString(url));
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            ForecastioReponse dataObject = serializer.Deserialize<ForecastioReponse>(stringResult);
+
+            return dataObject;
+            
+        }
+
+        private string FormatResponse(string _input)
+        {
+            _input = _input.Replace("isd-stations", "isd_stations");
+            _input = _input.Replace("lamp-stations", "lamp_stations");
+            _input = _input.Replace("metar-stations", "metar_stations");
+            _input = _input.Replace("darksky-stations", "darksky_stations");
+            return _input;
+        }
+    }
+}
