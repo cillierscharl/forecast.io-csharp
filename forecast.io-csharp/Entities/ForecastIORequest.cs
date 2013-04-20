52bf7dc7e2317425bcd28852156f1fb56959e86c
Charl Cilliers <cilliers.charl@gmail.com>
2013-04-20 20:07:38 +0200

    Introduced Helper class to reuse functions.

--- forecast.io-csharp/Entities/ForecastIORequest.cs
+++ forecast.io-csharp/Entities/ForecastIORequest.cs
@@ -14,37 +14,25 @@ namespace ForecastIO
         private string exclude;
         private string time;
 
-        private string currentForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&exclude={4}";
-        private string periodForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2},{3}?units={4}&exclude={5}";
+        private static string currentForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&exclude={4}";
+        private static string periodForecastURL = "https://api.forecast.io/forecast/{0}/{1},{2},{3}?units={4}&exclude={5}";
 
         public ForecastIOReponse Get()
         {
-            var client = new WebClient();
-            var url = "";
-            if (time == null)
+            var url = (time == null) ? String.Format(currentForecastURL, apiKey, latitude, longitude, unit, exclude) :
+                String.Format(periodForecastURL, apiKey, latitude, longitude, time, unit, exclude);
+
+            string result;
+            using (WebClient client = new WebClient())
             {
-                url = String.Format(currentForecastURL, apiKey, latitude, longitude, unit, exclude);
-            }
-            else
-            {
-                url = String.Format(periodForecastURL, apiKey, latitude, longitude, time, unit, exclude);
+                result = RequestHelpers.FormatResponse(client.DownloadString(url));
             }
 
-            var stringResult = FormatResponse(client.DownloadString(url));
-            JavaScriptSerializer serializer = new JavaScriptSerializer();
-            ForecastIOReponse dataObject = serializer.Deserialize<ForecastIOReponse>(stringResult);
+            var serializer = new JavaScriptSerializer();
+            var dataObject = serializer.Deserialize<ForecastIOReponse>(result);
 
             return dataObject;
             
-        }
-
-        private string FormatResponse(string _input)
-        {
-            _input = _input.Replace("isd-stations", "isd_stations");
-            _input = _input.Replace("lamp-stations", "lamp_stations");
-            _input = _input.Replace("metar-stations", "metar_stations");
-            _input = _input.Replace("darksky-stations", "darksky_stations");
-            return _input;
         }
 
         public ForecastIORequest(string _apiKey, float _lat, float _long, string _unit, params string[] _exclude)
@@ -53,18 +41,7 @@ namespace ForecastIO
             latitude = _lat.ToString(CultureInfo.InvariantCulture);
             longitude = _long.ToString(CultureInfo.InvariantCulture);
             unit = _unit;
-            if (_exclude.Length > 0)
-            {
-                foreach (string excludeBlock in _exclude)
-                {
-                    exclude += excludeBlock + ",";
-                }
-                exclude = exclude.TrimEnd(',');
-            }
-            else
-            {
-                exclude = "";
-            }
+            exclude = RequestHelpers.FormatExcludeString(_exclude);
         }
 
         public ForecastIORequest(string _apiKey, float _lat, float _long, DateTime _time, string _unit, params string[] _exclude)
@@ -72,22 +49,9 @@ namespace ForecastIO
             apiKey = _apiKey;
             latitude = _lat.ToString(CultureInfo.InvariantCulture);
             longitude = _long.ToString(CultureInfo.InvariantCulture);
-            var milliseconds = _time.ToUniversalTime().Subtract(
-                new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-            time = Convert.ToInt64(milliseconds).ToString();
+            time = RequestHelpers.FormatUTCString(_time);
             unit = _unit;
-            if (_exclude.Length > 0)
-            {
-                foreach (string excludeBlock in _exclude)
-                {
-                    exclude += excludeBlock + ",";
-                }
-                exclude = exclude.TrimEnd(',');
-            }
-            else
-            {
-                exclude = "";
-            }
+            exclude = RequestHelpers.FormatExcludeString(_exclude);
         }
     }
 }
---
+++ forecast.io-csharp/Helpers/RequestHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForecastIO
+{
+    public static class RequestHelpers
+    {
+        public static string FormatResponse(string _input)
+        {
+            _input = _input.Replace("isd-stations", "isd_stations");
+            _input = _input.Replace("lamp-stations", "lamp_stations");
+            _input = _input.Replace("metar-stations", "metar_stations");
+            _input = _input.Replace("darksky-stations", "darksky_stations");
+            return _input;
+        }
+
+        public static string FormatExcludeString(string[] _input)
+        {
+            var output = "";
+            if (_input.Length > 0)
+            {
+                foreach (string excludeBlock in _input)
+                {
+                    output += excludeBlock + ",";
+                }
+                output = output.TrimEnd(',');
+            }
+            else
+            {
+                output = "";
+            }
+
+            return output;
+        }
+
+        public static string FormatUTCString(DateTime _input)
+        {
+            var milliseconds = _input.ToUniversalTime().Subtract(
+                new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+
+            return Convert.ToInt64(milliseconds).ToString();
+        }
+    }
+}
