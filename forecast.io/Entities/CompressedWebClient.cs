aa1012e511aeb588bc12658084413cffbcbbd7ec
Michael-0 <michael.danilyuk@gmail.com>
2013-11-30 05:08:07 +0100

    Fix default request.Proxy setting
    Prevents a possible 10+ sec delay (See http://stackoverflow.com/questions/2519655/httpwebrequest-is-extremely-slow/3603413#3603413)

--- forecast.io/Entities/CompressedWebClient.cs
+++ forecast.io/Entities/CompressedWebClient.cs
@@ -9,6 +9,7 @@ namespace ForecastIO
         {
             var request = base.GetWebRequest(address) as HttpWebRequest;
             if (request == null) return null;
+            request.Proxy = null; //Prevents a possible 10+ sec delay (See http://stackoverflow.com/questions/2519655/httpwebrequest-is-extremely-slow/3603413#3603413)
             request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
             return request;
         }
