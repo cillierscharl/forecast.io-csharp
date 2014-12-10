f71725b313daf48569b3b035bd410d7ee1ba327d
Tomek Iniewicz <stuff@itomek.com>
2014-12-10 11:43:02 -0500

    Added comments, clean-up, made request testable.

--- forecast.io/Entities/IForecastIORequest.cs
+++ forecast.io/Entities/IForecastIORequest.cs
@@ -1,11 +1,80 @@
-﻿namespace ForecastIO
+namespace ForecastIO
 {
+    using System;
+
+    /// <summary>
+    /// The ForecastIORequest interface.
+    /// </summary>
     public interface IForecastIORequest
     {
-        ForecastIOResponse Get();
+        /// <summary>
+        /// Retrieves the forecast for the give latitude, longitude and unit.
+        /// </summary>
+        /// <param name="latitude">
+        /// The latitude.
+        /// </param>
+        /// <param name="longitude">
+        /// The longitude.
+        /// </param>
+        /// <param name="unit">
+        /// The unit (i.e, US).
+        /// </param>
+        /// <param name="extend">
+        /// Extended options.
+        /// </param>
+        /// <param name="exclude">
+        /// All exclusions.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ForecastIOResponse"/>.
+        /// </returns>
+        ForecastIOResponse Get(
+            float latitude,
+            float longitude,
+            Unit unit,
+            Extend[] extend = null,
+            Exclude[] exclude = null);
 
+        /// <summary>
+        /// Retrieves the forecast for the give latitude, longitude and unit.
+        /// </summary>
+        /// <param name="latitude">
+        /// The latitude.
+        /// </param>
+        /// <param name="longitude">
+        /// The longitude.
+        /// </param>
+        /// <param name="time">
+        /// The time.
+        /// </param>
+        /// <param name="unit">
+        /// The unit (i.e, US).
+        /// </param>
+        /// <param name="extend">
+        /// Extended options.
+        /// </param>
+        /// <param name="exclude">
+        /// All exclusions.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ForecastIOResponse"/>.
+        /// </returns>
+        ForecastIOResponse Get(
+            float latitude,
+            float longitude,
+            DateTime? time,
+            Unit unit,
+            Extend[] extend = null,
+            Exclude[] exclude = null);
+
+        /// <summary>
+        /// Gets the API calls made.
+        /// </summary>
         string ApiCallsMade { get; }
 
+        /// <summary>
+        /// Gets the API response time.
+        /// </summary>
         string ApiResponseTime { get; }
     }
 }
